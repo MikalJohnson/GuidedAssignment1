e0fe8362c18156a7f687a91b5f35cb61d1ca56f9
MikalJohnson <Johnsonmikal24@gmail.com>
2024-01-08 19:45:18 -0800

    Add project files.

---
+++ GuidedAssignment1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GuidedAssignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
